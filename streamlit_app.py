c91a48c3e6b827787470198eb1e3bcff0b7520cf
MiHyang <doyouloveangela@chunchon.gwe.hs.kr>
2025-11-18 14:40:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,6 +75,9 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
     # },
     labels[0] : {"texts" : ["중국식 냉면은 맛있어"]},
+    labels[1] : {"texts" : ["짜장면은 맛있어"]},
+    labels[2] : {"texts" : ["짬뽕은 맛있어"]},
+    labels[3] : {"texts" : ["탕수육은 맛있어"]},
 }
 
 # ======================
